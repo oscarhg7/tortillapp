390290c7d48d436058d1a6ca421f6b4dbd776ab0
Oscarhg_7 <oscarhernandezgallego@gmail.com>
2026-03-02 20:18:29 +0100

    Primera subida de TortillApp

--- app.py
+++ app.py
@@ -222,4 +222,4 @@ def select_location():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
